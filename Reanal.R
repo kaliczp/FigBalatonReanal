b6d16fdc1d905684dd94fd79752e267ee5b42448
Péter Kalicz <kaliczp@gmail.com>
2019-10-18 18:45:06 +0200

    Temperature and water level added

--- Reanal.R
+++ Reanal.R
@@ -1,5 +1,7 @@
 ggplot(Bal.xts, aes(x=Index, y=Prec)) +
-    geom_bar(stat="identity", fill="lightblue") +
+    geom_bar(stat="identity", fill ="lightblue") +
     labs(x="", y="Precipitation [mm]") +
     scale_y_reverse() +
-    expand_limits(y=c(0, 1000))
+    expand_limits(y=c(0, 1500)) +
+    geom_line(aes(y = (1500 - Temp*80))) +
+    geom_line(aes(y = (1500 - WLev*10)))
