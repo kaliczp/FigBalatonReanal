f1b6c8b632254df1f5c8cd3a3cd570dcc9912518
Péter Kalicz <kaliczp@gmail.com>
2019-10-18 20:13:20 +0200

    Parametric scale customised axes

--- Reanal.R
+++ Reanal.R
@@ -1,7 +1,14 @@
+Max.scale <- 2000
 ggplot(Bal.xts, aes(x=Index, y=Prec)) +
     geom_bar(stat="identity", fill ="lightblue") +
-    labs(x="", y="Precipitation [mm]") +
-    scale_y_reverse() +
-    expand_limits(y=c(0, 1500)) +
-    geom_line(aes(y = (1500 - Temp*80))) +
-    geom_line(aes(y = (1500 - WLev*10)))
+    labs(x="") +
+    scale_y_reverse(name = "", breaks = c(0,500,1000, 1500, 2000),
+                    labels = c(0, 500,1000,10,5),
+                    sec.axis = sec_axis(~.*10, name = "Water level [cm]",
+                                        breaks = c(5000,10000,15000,20000),
+                                        labels = c(300,200,100,0))
+                    ) +
+    expand_limits(y=c(0, Max.scale)) +
+    geom_line(aes(y = (Max.scale - Temp*100 + 500)), colour = "red") +
+    geom_line(aes(y = (Max.scale - WLev*10)), colour = "blue")
+
